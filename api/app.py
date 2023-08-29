0497c5ebc233b20a8d6fa8b4d9162b274c8b0311
Ayush Jain <ayushganna67@gmail.com>
2023-08-29 20:16:39 +0530

    test

--- api/app.py
+++ api/app.py
@@ -8,10 +8,11 @@ def get_svg():
     if not package_name:
         return "Missing package_name parameter", 400
     
-    svg_content = generate_svg.make_widget(package_name)
+    #svg_content = generate_svg.make_widget(package_name)
     
-    response = Response(svg_content, content_type='image/svg+xml')
-    return response
+    # response = Response(svg_content, content_type='image/svg+xml')
+    # return response
+    return package_name
     # return send_file(f"{package_name}.svg", mimetype='image/svg+xml')
 if __name__ == '__main__':
     app.run(debug=True)
